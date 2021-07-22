55dc1b0d14ec47337f17acb0862b64d92c89fa83
Maksym Dzhafarov <maksim8797@gmail.com>
2021-07-22 17:12:13 +0300

    bug fixed

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,14 +1,12 @@
 export default { fetchCountries };
 
-const URL = 'https://restcountries.eu/rest/v2';
+const URL = 'https://restcountries.eu/rest/v2/name/';
 
 function fetchCountries(name) {
-  return fetch(`${URL}/name/${name}?fields=name;capital;population;languages;flag`).then(
-    response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    },
-  );
+  return fetch(`${URL}${name}?fields=name;capital;population;languages;flag`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
